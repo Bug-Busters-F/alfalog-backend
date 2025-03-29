0aa771fa2abe5d863c458ca43891b0e63d5226c1
Wesley Gonçalves <dev@wesleygoncalves.com>
2025-03-29 16:55:58 -0300

    feat: separa limite de tamanho de lotes dos importers
    LIMIT refere-se ao número máximo de linhas que um importer irá exportar
    BATCH_SIZE: a cada BATCH_SIZE linhas, um importer irá comitar as linhas lidas

--- src/importers/urfs.py
+++ src/importers/urfs.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from ..urfs.model import URFModel
 from ..utils.sqlalchemy import SQLAlchemy
-from . import BATCH_SIZE
+from . import BATCH_SIZE, LIMIT
 
 baseurl = "https://balanca.economia.gov.br/balanca/bd/tabelas/URF.csv"
 
@@ -32,6 +32,8 @@ def importar(replace: bool = False):
         # Commit in batches
         if index % BATCH_SIZE == 0:
             db.session.commit()
+        if index == LIMIT:
+            break
 
     db.session.commit()
 
