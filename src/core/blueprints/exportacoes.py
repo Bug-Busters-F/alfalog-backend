0882ec45b7f480fe23e182bdb9d97cc525ab70c1
Allan Couto <allan.leste@hotmail.com>
2025-04-26 13:10:54 -0300

    feat: paginação dentro de exportações

--- src/core/blueprints/exportacoes.py
+++ src/core/blueprints/exportacoes.py
@@ -1,6 +1,8 @@
 import marshal
+import time
 from flask import Blueprint, request, jsonify
 from flask_restful import marshal_with, marshal
+from sqlalchemy import func
 from ..request import valor_agregado_args, cargas_movimentadas_args
 from ..fields import cargas_movimentadas_fields, valor_agregado_fields
 from src.exportacoes.model import ExportacaoModel
@@ -14,44 +16,66 @@ exportacoes = Blueprint("exportacoes", __name__)
 @exportacoes.route("/api/exportacoes/valor-agregado", methods=["POST"])
 @marshal_with(valor_agregado_fields)
 def valor_agregado():
-    """Retrieve Transações incluindo seu valor agregado."""
-    # input validation
-    args = valor_agregado_args.parse_args(strict=True)
+    start_time = time.time()
 
+    args = valor_agregado_args.parse_args(strict=True)
     db = SQLAlchemy.get_instance()
 
     page = args["page"] or 1
     per_page = args["per_page"] or 20
+
+    page = max(1, page)
+    per_page = max(1, per_page)
     offset = (page - 1) * per_page
 
-    base_query = (
+    # Fazendo a pre contagem dos dados antes de forma mais rapida
+    total = (
+        db.session.query(func.count(ExportacaoModel.id))
+        .join(UFModel)
+        .filter(UFModel.id == args["uf_id"], ExportacaoModel.ano == args["ano"])
+        .scalar()
+    )
+
+    # Pegando os itens conforme o necessario
+    items = (
         db.session.query(ExportacaoModel)
         .join(UFModel)
         .filter(UFModel.id == args["uf_id"], ExportacaoModel.ano == args["ano"])
         .order_by(ExportacaoModel.valor_agregado.desc())
+        .limit(per_page)
+        .offset(offset)
+        .all()
     )
-
-    total = base_query.count()
-    items = base_query.limit(per_page).offset(offset).all()
 
     response = {
         "total": total,
         "page": page,
         "per_page": per_page,
-        "items": marshal(items, valor_agregado_fields),
+        "items": items, 
     }
 
-    return jsonify(response)
+    elapsed_time = time.time() - start_time
+    print(f"Tempo da requisição: {elapsed_time:.4f} segundos")
+
+    return response
+
 
 
 @exportacoes.route("/api/exportacoes/cargas-movimentadas", methods=["POST"])
 @marshal_with(cargas_movimentadas_fields)
 def cargas_movimentadas():
+    db = SQLAlchemy.get_instance()
     """Inclui dados referente as cargas movimentadas."""
     # input validation
     args = cargas_movimentadas_args.parse_args(strict=True)
 
-    db = SQLAlchemy.get_instance()
+    # Sessão para paginação
+    page = args["page"] or 1
+    per_page = args["per_page"] or 20
+
+    page = max(1, page)
+    per_page = max(1, per_page)
+    offset = (page - 1) * per_page
 
     entries = (
         db.session.query(
@@ -62,7 +86,24 @@ def cargas_movimentadas():
         .join(UFModel)
         .filter(UFModel.id == args["uf_id"], ExportacaoModel.ano == args["ano"])
         .order_by(db.desc(ExportacaoModel.peso))
+        .limit(per_page)
+        .offset(offset)
         .all()
     )
 
-    return entries
+    total = (
+        db.session.query(func.count(ExportacaoModel.id))
+        .join(UFModel)
+        .filter(UFModel.id == args["uf_id"], ExportacaoModel.ano == args["ano"])
+        .scalar()
+    )
+
+
+    response = {
+        "total": total,
+        "page": page,
+        "per_page": per_page,
+        "cargas": entries, 
+    }
+
+    return response
