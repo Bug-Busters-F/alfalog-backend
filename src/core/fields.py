e6b5b35794709573d38529753fa7a7ddb54f9c35
Allan Couto <allan.leste@hotmail.com>
2025-04-26 13:09:39 -0300

    feat: schema para paginação de cargas movimentadas

--- src/core/fields.py
+++ src/core/fields.py
@@ -5,9 +5,17 @@ from src.exportacoes.fields import model_fields as transacao_fields
 
 valor_agregado_fields = transacao_fields["data"]
 
+
+# Correção no retorno da requisição para suportar a paginação
 cargas_movimentadas_fields = {
-    "id": fields.Integer,
-    "peso": fields.Integer,
-    # FKs
-    "ncm_id": fields.Integer,
+    "total": fields.Integer,
+    "page": fields.Integer,
+    "per_page": fields.Integer,
+    "cargas": fields.List(fields.Nested({
+        "id": fields.Integer,
+        "peso": fields.Integer,
+        "ncm_id": fields.Integer,
+    }))
 }
+
+
