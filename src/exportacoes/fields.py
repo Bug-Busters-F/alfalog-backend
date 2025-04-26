376238b8857819f5c738c5005f43103c194ed589
Allan Couto <allan.leste@hotmail.com>
2025-04-26 13:11:35 -0300

    feat: modelo atualizado para paginação

--- src/exportacoes/fields.py
+++ src/exportacoes/fields.py
@@ -3,22 +3,27 @@
 from flask_restful import fields
 
 
+# Modelo de retorno ajustado para a paginação.
 model_fields = {
     "data": {
-        "id": fields.Integer,
-        "codigo": fields.String,
-        "nome": fields.String,
+    "total": fields.Integer,
+    "page": fields.Integer,
+    "per_page": fields.Integer,
+    "items": fields.List(fields.Nested({
         "ano": fields.Integer,
         "mes": fields.Integer,
         "peso": fields.Integer,
         "valor": fields.Integer,
         "valor_agregado": fields.Float,
-        # FKs
         "ncm_id": fields.Integer,
         "ue_id": fields.Integer,
         "pais_id": fields.Integer,
         "uf_id": fields.Integer,
         "via_id": fields.Integer,
         "urf_id": fields.Integer,
-    },
-}
+        "codigo": fields.String,
+        "nome": fields.String,
+    }))
+}}
+
+
