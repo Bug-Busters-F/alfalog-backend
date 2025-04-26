c01a04f12d924031878dba1068b355de30cb57e4
Allan Couto <allan.leste@hotmail.com>
2025-04-26 13:11:09 -0300

    feat: paginação dentro de  importações

--- src/core/blueprints/importacoes.py
+++ src/core/blueprints/importacoes.py
@@ -1,6 +1,7 @@
 import marshal
 from flask import Blueprint, request
 from flask_restful import marshal_with
+from sqlalchemy import func
 from ..request import valor_agregado_args, cargas_movimentadas_args
 from ..fields import valor_agregado_fields, cargas_movimentadas_fields
 from src.importacoes.model import ImportacaoModel
@@ -18,17 +19,42 @@ def valor_agregado():
     # input validation
     args = valor_agregado_args.parse_args(strict=True)
 
+
+    page = args["page"] or 1
+    per_page = args["per_page"] or 20
+
+    page = max(1, page)
+    per_page = max(1, per_page)
+    offset = (page - 1) * per_page
+
     db = SQLAlchemy.get_instance()
+
+    total = (
+        db.session.query(func.count(ImportacaoModel.id))
+        .join(UFModel)
+        .filter(UFModel.id == args["uf_id"], ImportacaoModel.ano == args["ano"])
+        .scalar()
+    )
 
     entries = (
         db.session.query(ImportacaoModel)
         .join(UFModel)
         .filter(UFModel.id == args["uf_id"], ImportacaoModel.ano == args["ano"])
         .order_by(ImportacaoModel.valor_agregado.desc())
+        .limit(per_page)
+        .offset(offset)
         .all()
     )
 
-    return entries
+    response = {
+        "total": total,
+        "page": page,
+        "per_page": per_page,
+        "items": entries, 
+    }
+
+
+    return response
 
 
 @importacoes.route("/api/importacoes/cargas-movimentadas", methods=["POST"])
@@ -38,7 +64,21 @@ def cargas_movimentadas():
     # input validation
     args = cargas_movimentadas_args.parse_args(strict=True)
 
+    page = args["page"] or 1
+    per_page = args["per_page"] or 20
+
+    page = max(1, page)
+    per_page = max(1, per_page)
+    offset = (page - 1) * per_page
+
     db = SQLAlchemy.get_instance()
+
+    total = (
+        db.session.query(func.count(ImportacaoModel.id))
+        .join(UFModel)
+        .filter(UFModel.id == args["uf_id"], ImportacaoModel.ano == args["ano"])
+        .scalar()
+    )
 
     entries = (
         db.session.query(
@@ -49,7 +89,17 @@ def cargas_movimentadas():
         .join(UFModel)
         .filter(UFModel.id == args["uf_id"], ImportacaoModel.ano == args["ano"])
         .order_by(db.desc(ImportacaoModel.peso))
+        .limit(per_page)
+        .offset(offset)
         .all()
     )
 
-    return entries
+    response = {
+        "total": total,
+        "page": page,
+        "per_page": per_page,
+        "cargas": entries, 
+    }
+
+    return response
+
